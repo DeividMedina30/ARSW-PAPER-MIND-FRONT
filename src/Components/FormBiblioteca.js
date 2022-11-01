b4aa41efae67702b0931fbfa9457b49ae5d2f940
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-11-01 11:39:25 -0500

    Post

--- src/Components/FormBiblioteca.js
+++ src/Components/FormBiblioteca.js
@@ -3,10 +3,76 @@ import axios from "axios";
 import '../Styles/FormBiblioteca.css';
 import Navbar from "./Navbar";
 
+
+function postForm(){
+	const baseURL = "https://paparmindarsw.herokuapp.com/api/bibliotecas";
+	const [data, setData] = useState({
+		titulo: "",
+		fechaCreacion: "",
+		fechaModificacion: "",
+		descripcion: ""
+	})
+	function handle(e){
+		const newTitle={...data}
+		newTitle[e.target.id] = e.target.value
+		setData(newTitle)
+		console.log(newTitle)
+	}
+	return(
+		<div class="Forma">
+			<Navbar/>
+			<div class="containerForm">
+				<h1>Ingrese los datos de la Biblioteca:</h1><br/>
+				<form class="was-validated" onSubmit={guardarDatos}>
+					<div class="form-group">
+						<label for="title-biblioteca">Título Biblioteca</label>
+						<input onChange={(e)=>handle(e)} value={data.titulo} name="titulo" type="text" class="form-control" id="titulo" placeholder="Ingrese un título" required/>
+						<div class="invalid-feedback">El título no puede ser vacío</div>
+					</div>
+					<div class="form-group">
+						<label for="title-biblioteca">Fecha creación</label>
+						<input onChange={(e)=>handle(e)} value={data.fechaCreacion} name="fechaCreacion" type="Date" class="form-control" id="fechaCreacion" placeholder="Ingrese un título" required/>
+						<div class="invalid-feedback">El título no puede ser vacío</div>
+					</div>
+					<div class="form-group">
+						<label for="title-biblioteca">Fecha modificacion</label>
+						<input onChange={(e)=>handle(e)} value={data.fechaModificacion} name="fechaModificacion" type="Date" class="form-control" id="fechaModificacion" placeholder="Ingrese un título" required/>
+						<div class="invalid-feedback">El título no puede ser vacío</div>
+					</div>
+					<div class="mb-3">
+						<label  for="validationTextarea">Descripción</label>
+						<textarea onChange={(e)=>handle(e)} value={data.descripcion} name="descripcion" class="form-control is-invalid" id="descripcion" placeholder="Describa la biblioteca" required></textarea>
+					</div>
+					<button type="button" class="btn btn-outline-success btn-lg btn-block" onClick={this.saveBiblioteca}>Crear</button>
+					<button type="button" class="btn btn-outline-danger btn-lg btn-block" onClick="/">Cancelar</button>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default postForm;
+
+
+
+
+
+
+
+
+
+
+/*
 const FormBiblioteca = () =>{
 
-	const baseURL = "https://paiper-mind-back.herokuapp.com/api/bibliotecas";
+	const baseURL = "https://paparmindarsw.herokuapp.com/api/bibliotecas";
 	//const [biblioteca, setBiblioteca] = useState({})
+
+	const saveBiblioteca = (e) => {
+		this.BibliotecaService.saveBiblioteca(this.state.biblioteca).then(data => {
+			console.log(data);
+		})
+	}
 
 	const guardarDatos = (e) =>{
 		e.preventDefault()
@@ -48,13 +114,13 @@ const FormBiblioteca = () =>{
 						<label  for="validationTextarea">Descripción</label>
 						<textarea name="descripcion" class="form-control is-invalid" id="validationDescription" placeholder="Describa la biblioteca" required></textarea>
 					</div>
-					<button type="submit" class="btn btn-outline-success btn-lg btn-block">Crear</button>
+					<button type="button" class="btn btn-outline-success btn-lg btn-block" onClick={this.saveBiblioteca}>Crear</button>
 					<button type="button" class="btn btn-outline-danger btn-lg btn-block" onClick="/">Cancelar</button>
 				</form>
 			</div>
 		</div>
 	)
-	/*
+	
 	<div class="form-group">
 					<label name="tipoBiblioteca" for="type-biblioteca" id="tipoBiblioteca">Tipo Biblioteca</label>
 						<select class="custom-select" required>
@@ -64,8 +130,8 @@ const FormBiblioteca = () =>{
 							<option name="tipoBiblioteca3" value="3" id="tipoBiblioteca3">Al azar</option>
 						</select>
 						<div class="invalid-feedback">Debe seleccionar un tipo de Biblioteca</div>
-					</div>*/
+					</div>
 
-}
+} 
 
-export default FormBiblioteca;
+export default FormBiblioteca; */
--- src/service/BibliotecaService.js
+++ src/service/BibliotecaService.js
@@ -1,12 +1,19 @@
 import axios from "axios";
-
+const baseURL = "https://paparmindarsw.herokuapp.com/api/bibliotecas";
 export class BibliotecaService{
-    baseURL = "https://paparmindarsw.herokuapp.com/api/bibliotecas";
     getAll(){
         return axios.get(this.baseURL).then(res => res.data);
     }
 
     save(biblioteca){
         return axios.post(this.baseURL, biblioteca).then(res => res.data);
+    }
+}
+
+export async function saveBiblioteca(bibliotecaData){
+    try {
+        return axios.post(this.baseURL,bibliotecaData).then(res => res.data);
+    } catch (error) {
+        console.log(error)
     }
 }
